0f867f6395a5f709783ea03ebfbb6028aafed437
Angelo Verlain <hey@vixalien.com>
2025-03-26 12:45:20 +0000

    added links to the navigation menu

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -3,21 +3,31 @@ function initialiseMenu() {
   const menuClose = document.getElementById("menu-close");
   const mobileMenu = document.getElementById("mobile-menu");
 
+  const menuLinks = document.getElementById("menu-links");
+
   let open = false;
+
+  function showMenu() {
+    menuOpen.classList.add("hidden");
+    menuClose.classList.remove("hidden");
+    mobileMenu.classList.remove("translate-x-full");
+    mobileMenu.classList.add("translate-x-0");
+  }
+
+  function hideMenu() {
+    menuOpen.classList.remove("hidden");
+    menuClose.classList.add("hidden");
+    mobileMenu.classList.add("translate-x-full");
+    mobileMenu.classList.remove("translate-x-0");
+  }
 
   function handleMenuButtonClicked() {
     open = !open;
-  
+
     if (open) {
-      menuOpen.classList.add("hidden");
-      menuClose.classList.remove("hidden");
-      mobileMenu.classList.remove("translate-x-full");
-      mobileMenu.classList.add("translate-x-0");
+      showMenu();
     } else {
-      menuOpen.classList.remove("hidden");
-      menuClose.classList.add("hidden");
-      mobileMenu.classList.add("translate-x-full");
-      mobileMenu.classList.remove("translate-x-0");
+      hideMenu();
     }
   }
 
@@ -25,7 +35,6 @@ function initialiseMenu() {
     menuOpen.addEventListener("click", handleMenuButtonClicked);
     menuClose.addEventListener("click", handleMenuButtonClicked);
   }
+
+  menuLinks.addEventListener("click", hideMenu, true);
 }
-
-initialiseMenu();
-
