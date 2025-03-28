bb15ef03dad40a7b913da8f28334b3cee13bc934
ruran8wa <p.ruran8wa@alustudent.com>
2025-03-28 09:24:03 +0200

    working on reviews
    solved

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -3,6 +3,8 @@ function initialiseMenu() {
   const menuClose = document.getElementById("menu-close");
   const mobileMenu = document.getElementById("mobile-menu");
 
+  const menuLinks = document.getElementById("menu-links");
+
   let open = false;
 
   function showMenu() {
@@ -23,15 +25,9 @@ function initialiseMenu() {
     open = !open;
 
     if (open) {
-      menuOpen.classList.add("hidden");
-      menuClose.classList.remove("hidden");
-      mobileMenu.classList.remove("translate-x-full");
-      mobileMenu.classList.add("translate-x-0");
+      showMenu();
     } else {
-      menuOpen.classList.remove("hidden");
-      menuClose.classList.add("hidden");
-      mobileMenu.classList.add("translate-x-full");
-      mobileMenu.classList.remove("translate-x-0");
+      hideMenu();
     }
   }
 
